a1cfcf15a9ec2bd9c938fb94f89e382f6644518d
sobralAllan <allan.sobraldasilva@gmail.com>
2022-08-25 21:37:35 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, Image, TextInput} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.janelaUm}>
+        <Text style={styles.titulo}>New Aplicativo em React Native</Text>
+      </View>
+
+      <View style={styles.janelaTres}>
+        <TextInput style={styles.campoNumericoUm} 
+        placeholder='Informe um número' 
+        keyboardType='numeric'/>
+      </View>
+
+      <View style={styles.janelaDois}>
+        <Button style={styles.botao}
+          title='Confirmar'
+          onPress={() => Alert.alert('Cadastro Realizado com Sucesso!')}
+        />
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +30,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3412aa',
+    color: '#4412aa',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    backgroundColor: '#99ffaa',
+    color: '#887766',
+    textAlign: 'center',
+    textAlignVertical: 'bottom',
+  },
+  botao:{
+    backgroundColor: '#001aa',
+    color: '#aaa',
+    width: '100%',
+  },
+  janelaUm: {
+    flex: 1,
+    border: '2px solid black',
+    backgroundColor: '#000999',
+    width: '100%',
+    borderColor: '#bbccdd',
+  },
+  janelaDois:{
+    marginTop: '100%',
+    width: '100%',
+    flex: 2,
+    border: '2px solid black',
+    backgroundColor: '#abc',
+    borderRadius: 50,
+    color: '#55aaff',
+  },
+  janelaTres:{
+    flex: 3,
+    backgroundColor: '#fff',
+    width: '100%',
+  },
+  campoNumericoUm:{
+    backgroundColor: '#000',
+    marginTop: '40%',
+    height: '21%',
+    color: '#fff',
+  },
+
+  
 });
